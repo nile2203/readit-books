7ef0f6c36b9e83a84c6eb50037a491577f6cfd4b
Nilesh Aggarwal <engnile2203@gmail.com>
2018-04-27 01:51:49 +0530

    Changes in models and admin file

--- books/admin.py
+++ books/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from books.models import Books
 
 # Register your models here.
+class BooksAdmin(admin.ModelAdmin):
+	list_display=('title','author')
+
+admin.site.register(Books, BooksAdmin)
--- books/models.py
+++ books/models.py
@@ -10,4 +10,12 @@ class Books(models.Model):
 	author = models.CharField(max_length=70, blank=False)
 	review = models.TextField(max_length=500, blank=True, null=True)
 	date_reviewed = models.DateTimeField(default=None, blank=True, null=True)
-	is_favourite = models.BooleanField(default=False)
+	is_favourite = models.BooleanField(default=False, verbose_name="Favourite")
+
+	def __unicode__(self):
+		return self.title + " " + self.author
+
+	class Meta:
+		verbose_name = "Book"
+		verbose_name_plural = "Books"
+
