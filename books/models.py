bebfd4005ed39f11e9ae15566c4f04c8068bc977
Nilesh Aggarwal <engnile2203@gmail.com>
2018-04-27 01:26:48 +0530

    First App created in django

---
+++ books/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Books(models.Model):
+	title = models.CharField(max_length=150, blank=False)
+	author = models.CharField(max_length=70, blank=False)
+	review = models.TextField(max_length=500, blank=True, null=True)
+	date_reviewed = models.DateTimeField(default=None, blank=True, null=True)
+	is_favourite = models.BooleanField(default=False)
